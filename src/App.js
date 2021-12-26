2544feb74289aab2baee2552b3981600c852e97b
BhanukaAGH <it20009472@my.sliit.lk>
2021-12-26 22:01:27 +0530

    add text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div></div>
+  return (
+    <div className='bg-gray-800 text-teal-500 flex justify-center items-center h-screen'>
+      <h1 className='xl:text-8xl md:text-6xl text-4xl font-mono font-black'>
+        GitHub Actions Practice
+      </h1>
+    </div>
+  )
 }
 
 export default App
